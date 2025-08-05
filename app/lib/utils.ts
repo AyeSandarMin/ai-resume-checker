74376dc4599db8dc7931acf9c05d5b2513b9346d
Aye Sandar Min <ayesandarmin.dev@gmail.com>
2025-08-05 21:09:46 +0700

    feat: fetch real resumes and show on the homepage

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -8,13 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 export function formatSize(bytes: number): string {
   if (bytes === 0) return "0 Bytes";
 
-  const units = ["Bytes", "KB", "MB", "GB", "TB"];
   const k = 1024;
-  const dm = 2;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + units[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
 }
 
 export const generateUUID = () => crypto.randomUUID();
